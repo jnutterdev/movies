a5be60c0dadecb8eb46eb376e609bdfd6d1e9a66
terribledevice <longlost@gmail.com>
2019-10-21 22:34:35 -0400

    Finished up app under yaml branch

--- app.rb
+++ app.rb
@@ -2,26 +2,29 @@ require 'sinatra'
 require 'movie'
 require 'movie_store'
 
+# Create a MovieStore that updates the movies.ynl file
 store = MovieStore.new('movies.yml')
 
 get('/movies') do
-    @movies = store.all
-    erb :index
+    @movies = store.all # Load all Movie objects from movies.yml
+    erb :index # Embed movies in HTML from views/index.erb and return it
 end
 
 get('/movies/new') do
-    erb :new
+    erb :new # Return HTML in views/new.erb
 end
 
-post('/movies/create') do
-    @movie = Movie.new
-    @movie.title = params['title']
-    @movie.director = params['director']
-    @movie.year = params['year']
-    store.save(@movie)
-    redirect 'movies/new'
+post('/movies/create') do # Submitted form data goes here
+    @movie = Movie.new # Create object to hold form data
+    @movie.title = params['title'] # Add the form data to the object
+    @movie.director = params['director'] # form data
+    @movie.year = params['year'] # form data
+    store.save(@movie) # Save the object!
+    redirect 'movies/new' # Show a new, empty form
 end
 
 get('/movies/:id') do
-    "Received a request for movie ID: #{params['id']}"
+    id = params['id'].to_i # Convert the 'id' parameter from a string to an integer
+    @movie = store.find(id) # Use the ID to load the movie from the store
+    erb :show # Embed the movie in the HTML from show.erb and return it to the browser
 end
